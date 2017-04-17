dc8bd8a53165ea5bea1e5d8a3998e72dade05b4e
胡正康 <huzhengkang@huzhengkangdeiMac.local>
2017-04-17 13:33:44 +0800

    Initial Commit

---
+++ ZYLeadingPage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYLeadingPage
+//
+//  Created by 胡正康 on 2017/4/17.
+//  Copyright © 2017年 ZY_kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZYLeadingPage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZYLeadingPage
+//
+//  Created by 胡正康 on 2017/4/17.
+//  Copyright © 2017年 ZY_kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
